e4b342ecaa7c063ba6ffd9f57fab9e2060bfb4f2
Saugat Dahal <saugat.dahal@np.technofex.com>
2026-05-19 10:21:45 +0545

    bank transa

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BankTransferMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
